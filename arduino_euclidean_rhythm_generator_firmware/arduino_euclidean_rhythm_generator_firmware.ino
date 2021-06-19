8120281fb97af3177fcdd4219ef67b3117b085df
TimMJN <75533580+TimMJN@users.noreply.github.com>
2021-06-19 20:19:27 +0200

    Create arduino_euclidean_rhythm_generator_firmware.ino

---
+++ arduino_euclidean_rhythm_generator_firmware/arduino_euclidean_rhythm_generator_firmware.ino
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+   Arduino Euclidean Rhythm Generator
+   synthesizer module firmware
+
+   by TimMJN
+
+   v0.1
+   19-06-2021
+
+   For schematics and other information, see
+   https://github.com/TimMJN/Arduino-Euclidean-Rhythm-Generator
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
